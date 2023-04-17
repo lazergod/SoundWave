1a54b962ba0533ebc8880341bd8886dd98c59607
Jairo Herrera <81483549+lazergod@users.noreply.github.com>
2023-04-16 20:09:20 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace SoundWave
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.tmrProgress = new System.Windows.Forms.Timer(this.components);
+            this.lblElapsed = new System.Windows.Forms.Label();
+            this.lblDuration = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(12, 100);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(125, 75);
+            this.btnPlay.TabIndex = 1;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 393);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(776, 45);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(549, 230);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(125, 75);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(196, 244);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(125, 75);
+            this.btnPrevious.TabIndex = 3;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(352, 130);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(125, 75);
+            this.btnPause.TabIndex = 4;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(327, 9);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(15, 16);
+            this.lblFileName.TabIndex = 5;
+            this.lblFileName.Text = "--";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 50);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(776, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // tmrProgress
+            // 
+            this.tmrProgress.Interval = 1000;
+            this.tmrProgress.Tick += new System.EventHandler(this.tmrProgress_Tick);
+            // 
+            // lblElapsed
+            // 
+            this.lblElapsed.AutoSize = true;
+            this.lblElapsed.Location = new System.Drawing.Point(268, 89);
+            this.lblElapsed.Name = "lblElapsed";
+            this.lblElapsed.Size = new System.Drawing.Size(38, 16);
+            this.lblElapsed.TabIndex = 7;
+            this.lblElapsed.Text = "00:00";
+            // 
+            // lblDuration
+            // 
+            this.lblDuration.AutoSize = true;
+            this.lblDuration.Location = new System.Drawing.Point(344, 89);
+            this.lblDuration.Name = "lblDuration";
+            this.lblDuration.Size = new System.Drawing.Size(38, 16);
+            this.lblDuration.TabIndex = 8;
+            this.lblDuration.Text = "00:00";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(77, 254);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(125, 75);
+            this.btnStop.TabIndex = 9;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click_1);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.Red;
+            this.btnSalir.Location = new System.Drawing.Point(720, 141);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(125, 40);
+            this.btnSalir.TabIndex = 10;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::SoundWave.Properties.Resources.back;
+            this.ClientSize = new System.Drawing.Size(807, 203);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.lblDuration);
+            this.Controls.Add(this.lblElapsed);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SoundWave";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer tmrProgress;
+        private System.Windows.Forms.Label lblElapsed;
+        private System.Windows.Forms.Label lblDuration;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using AxWMPLib;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SoundWave
+{
+    public partial class Form1 : Form
+    {
+        string[] musicFiles;
+        int currentSong = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Deshabilitar el botón de maximizar en la barra de título del formulario
+            this.MaximizeBox = false;
+
+            // Establecer el tamaño del formulario
+            this.Size = new Size(650, 200);
+
+            // Establecer la carpeta de música y buscar archivos MP3
+            string musicFolderPath = @"ArchivosMusica";
+            musicFiles = Directory.GetFiles(musicFolderPath, "*.mp3");
+
+            // Establecer la primera canción si se encuentran archivos MP3
+            if (musicFiles.Length > 0)
+            {
+                axWindowsMediaPlayer1.URL = musicFiles[0];
+                lblFileName.Text = Path.GetFileName(musicFiles[0]);
+            }
+
+            //Ocultar el Media Player para personalizar el diseño
+            axWindowsMediaPlayer1.Visible = false;
+
+            // Iniciar el temporizador de progreso
+            tmrProgress.Start();
+
+            // Ubicar y cambiar el tamaño del label con el nombre de la canción
+            lblFileName.Location = new Point(10, 10);
+            lblFileName.AutoSize = true;
+
+            // Ubicar y cambiar el tamaño de la progress bar
+            progressBar1.Location = new Point(10, lblFileName.Bottom + 10);
+            progressBar1.Size = new Size(this.ClientSize.Width - 20, 20);
+
+            // Ubicar y cambiar el tamaño de los segundos transcurridos y la duración total
+            lblElapsed.Location = new Point(10, progressBar1.Bottom + 10);
+            lblElapsed.AutoSize = true;
+            lblDuration.Location = new Point(this.ClientSize.Width - lblDuration.Width - 10, progressBar1.Bottom + 10);
+            lblDuration.AutoSize = true;
+
+            // Ubicar y cambiar el tamaño de los controles para pausar, retroceder, etc.
+            btnPrevious.Location = new Point(10, lblElapsed.Bottom + 10);
+            btnPlay.Location = new Point(btnPrevious.Right + 10, lblElapsed.Bottom + 10);
+            btnPause.Location = new Point(btnPlay.Right + 10, lblElapsed.Bottom + 10);
+            btnStop.Location = new Point(btnPause.Right + 10, lblElapsed.Bottom + 10);
+            btnNext.Location = new Point(btnStop.Right + 10, lblElapsed.Bottom + 10);
+        }
+
+        ////Este método se llama cada vez que cambia la canción actual, 
+        ////Actualiza la etiqueta Label con el nombre del archivo de música actual
+        ////para que el usuario pueda ver qué canción está reproduciendo.
+        private void axWindowsMediaPlayer1_MediaChange(object sender, AxWMPLib._WMPOCXEvents_MediaChangeEvent e)
+        {
+            // Obtener el nombre del archivo actual
+            string fileName = axWindowsMediaPlayer1.currentMedia.name;
+
+            // Actualizar la etiqueta Label con el nombre del archivo
+            lblFileName.Text = Path.GetFileName(fileName);
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.pause();
+        }
+
+        private void btnStop_Click_1(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (currentSong > 0)
+            {
+                currentSong--;
+                axWindowsMediaPlayer1.URL = musicFiles[currentSong];
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+                lblFileName.Text = Path.GetFileName(musicFiles[currentSong]);
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (currentSong < musicFiles.Length - 1)
+            {
+                currentSong++;
+                axWindowsMediaPlayer1.URL = musicFiles[currentSong];
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+                lblFileName.Text = Path.GetFileName(musicFiles[currentSong]);
+            }
+        }
+
+        // Calcula el porcentaje de progreso de la canción actual y lo usa para actualizar la barra de progreso. 
+        // También muestra el tiempo transcurrido y la duración total de la canción actual en etiquetas Label.
+        private void tmrProgress_Tick(object sender, EventArgs e)
+        {
+            // Obtener la duración total del archivo
+            double duration = axWindowsMediaPlayer1.currentMedia.duration;
+
+            // Obtener la posición actual de reproducción
+            double position = axWindowsMediaPlayer1.Ctlcontrols.currentPosition;
+
+            // Calcular el porcentaje de progreso
+            int progress = (int)((position / duration) * 100);
+
+            // Actualizar la barra de progreso
+            progressBar1.Value = progress;
+
+            // Actualizar el tiempo transcurrido y la duración total
+            TimeSpan elapsed = TimeSpan.FromSeconds(position);
+            TimeSpan remaining = TimeSpan.FromSeconds(duration);
+            lblElapsed.Text = $"{elapsed:mm\\:ss}";
+            lblDuration.Text = $"{remaining:mm\\:ss}";
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
